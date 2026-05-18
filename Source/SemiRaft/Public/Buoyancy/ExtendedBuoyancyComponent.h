39512a465f4ff7f462501d5d55ea03ce61052f4e
LumenShurima <166148864+LumenShurima@users.noreply.github.com>
2026-05-18 20:49:01 +0900

    FIX: 누락된 인벤토리 코드 부분

---
+++ Source/SemiRaft/Public/Buoyancy/ExtendedBuoyancyComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BuoyancyComponent.h"
+#include "ExtendedBuoyancyComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEMIRAFT_API UExtendedBuoyancyComponent : public UBuoyancyComponent
+{
+	GENERATED_BODY()
+	
+};
